e25d73e70b311728eb04a46c9ea14bfbe8d1701a
SecurityQQ <flesh.game@mail.ru>
2015-10-11 21:09:11 +0300

    Working at redesigning graphs

---
+++ NetworkGraphs/NetworkGraphs/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NetworkGraphs
+//
+//  Created by Alexander Malyshev on 11.10.15.
+//  Copyright © 2015 Alexander Malyshev. All rights reserved.
+//
+
+#include <iostream>
+#include "IterableObject.h"
+
+int main(int argc, const char * argv[]) {
+    std::vector<int> asdf {5, 6, 7};
+    
+    
+    
+    return 0;
+}
