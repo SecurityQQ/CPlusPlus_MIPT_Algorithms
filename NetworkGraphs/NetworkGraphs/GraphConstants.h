e15643f8381d290e68caa2f8550d2c558c1a7029
SecurityQQ <flesh.game@mail.ru>
2015-10-21 00:19:05 +0300

    shitty working

---
+++ NetworkGraphs/NetworkGraphs/GraphConstants.h
@@ -0,0 +1,16 @@
+//
+//  GraphConstants.h
+//  SmartGraphs
+//
+//  Created by Alexander Malyshev on 04.10.15.
+//  Copyright © 2015 Alexander Malyshev. All rights reserved.
+//
+
+#ifndef GraphConstants_h
+#define GraphConstants_h
+
+#define USED_VERTEX (INT_MAX - 1)
+typedef unsigned long long VertexType;
+typedef long long VertexWeight;
+
+#endif /* GraphConstants_h */
